713f045b9c62dd16d3ecf72351e8d5179d29e05a
yassinederra77 <yassinederra779@gmail.com>
2026-03-20 20:52:20 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,6 +3,8 @@ from database import get_connection, get_system_status
 
 def login():
     st.markdown("<h2 style='text-align: center;'>🏫 Al Masab Service</h2>", unsafe_allow_html=True)
+    st.markdown("<div style='position:fixed; bottom:20px; left:0; width:100%; text-align:center; font-size:16px; font-weight:bold; color:black;'>💻 VERSION DE SYSTÈMES 1.0.0</div>", unsafe_allow_html=True)
+
 
     login_input = st.text_input("Login")
     password = st.text_input("Password", type="password")
